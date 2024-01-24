f461c63750f9435c3187874d4feb7587d402aac2
Faiem-qy <f.quanyoung@gmail.com>
2024-01-24 00:11:31 -0500

    add job postings

---
+++ db/seeds/02_job_postings.sql
@@ -0,0 +1,7 @@
+INSERT INTO job_postings (user_id, feedback_id, title, type_of_worker, rate, gender, duration, date, start_time, facility_name, facility_short_address, facility_latitude, facility_longitude, available_to_choose, is_filled) VALUES
+(1, NULL, 'Registered Nurse', 'Full-time', 50, 'Any', 'contract', '2024-02-01', '09:00:00', 'Elysium Medical Center', '123 Tech St', 40.7128, -74.0060, TRUE, FALSE),
+(2, NULL, 'Personal Service Worker', 'Part-time', 33, 'Female', 'Contract', '2024-02-15', '10:00:00', 'Aurelia Medical Group', '456 Design Ave'37.7749, -122.4194, TRUE, FALSE),
+(3, NULL, 'Registered Nurse', 'Full-time', 65, 'Male', 'contract', '2024-03-01', '08:30:00', 'Veritas Health Systems', '202 Analyze Ave', 41.8781, -87.6298, TRUE, FALSE);
+(4, NULL, 'Personal Service Worker', 'Full-time', 35, 'Any', 'contract', '2024-02-15', '09:30:00', 'Aurelia Medical Group', '456 Design Ave', 37.7749, -122.4194, TRUE, FALSE),
+(5, NULL, 'Registered Nurse', 'Part-time', 60, 'Any', 'Contract', '2024-03-01', '11:00:00', 'Elysium Medical Center', '123 Tech St', 40.7128, -74.0060, TRUE, FALSE),
+(6, NULL, 'Personal Service Worker', 'Full-time', 40, 'Any', 'contract', '2024-03-15', '08:00:00', 'Veritas Health Systems', '202 Analyze Ave'41.8781, -87.6298, TRUE, FALSE);
