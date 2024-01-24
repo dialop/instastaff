9427d1b9a4eb54026fa6b36fc4d8f6fa710a9515
Faiem-qy <f.quanyoung@gmail.com>
2024-01-24 00:21:16 -0500

    add messages sql

---
+++ db/seeds/03_messages.sql
@@ -0,0 +1,4 @@
+INSERT INTO messages (user_id, job_posting_id, text, date_sent, facility_name) VALUES
+(1, 1, 'Hi there, I would like to inquire about the patient-to-nurse ratio for this unit?', '2023-01-10 15:00:00', 'Elysium Medical Center'),
+(2, 2, 'Good day, could you share if there are any specific uniform or dress code requirements for this role?', '2023-01-11 16:00:00', 'Veritas Health Systems'),
+(3, 3, 'Hello, I am interested in the nursing position posted. Could you please provide more details about the role?"', '2023-01-12 17:00:00', 'Aurelia Medical Group');
