9c25073782605adcfe7e38f8c6f4348a6d74e581
dialop <dianalop1998@gmail.com>
2024-01-25 23:47:32 -0500

    Add env file

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import MapComponent from './components/MapComponent';
 import "./App.css";
 
+
 function App() {
   return (
-    <div>
-      <h1> Map</h1>
-      <MapComponent location="Toronto, ON" />
+    <div className="App">
+      <h1 className="text-xl font-bold mb-4">Map</h1>
+      <div className="p-4 bg-gray-100">
+        <MapComponent location="Toronto, ON" />
+      </div>
     </div>
   );
 }
+
 
 export default App;
