d9625f36dd9df66c289e64c06ee60010f4016ffc
Faiem-qy <f.quanyoung@gmail.com>
2024-01-25 22:35:51 -0500

    setup and populate navbar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Navbar from "./components/Nav";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <BrowserRouter>
+     <Navbar/>
+      <Routes> 
+        <Route path="/" element={<App/>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
