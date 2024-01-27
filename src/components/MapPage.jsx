8578671cdb4dd64fd94b1aa0337c1ffa9dd197ca
dialop <dianalop1998>
2024-01-27 15:39:26 -0500

    :lipstick:  Nav Bar and Maps
    Add nav to maps component & add location line to

--- src/components/MapPage.jsx
+++ src/components/MapPage.jsx
@@ -1,12 +1,17 @@
 // - MAP PAGE -//
 
 import React from 'react';
-import MapComponent from './MapComponent'; 
+import MapComponent from './MapComponent';
+import Navbar from './Nav';
+
+
+
 const MapPage = () => {
   const location = { lat: 43.6532, lng: -79.3832 }; // toronto coordinates 
 
   return (
     <div>
+      <Navbar/>
       <h1>Maps</h1>
       <MapComponent location={location} />
     </div>
