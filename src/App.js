e5204eed60321e02ab9ad9966ed68af39c6f8710
dialop <dianalop1998@gmail.com>
2024-01-27 18:31:02 -0500

    :sparkles: Add polyline to map
    Add polyline and shade center map

--- src/App.js
+++ src/App.js
@@ -1,17 +1,63 @@
 // - MAIN COMPONENT - //
 
-import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Nav';
+import MapComponent from './components/MapComponent';
 
-function App() {
-  useEffect(() => {
-  }, []);
 
+function Home() {
   return (
     <div>
-      <Navbar />
+      {}
     </div>
+  );
+}
+
+// center of the polyline
+function calculateCenter(borders) {
+  let latSum = 0;
+  let lngSum = 0;
+  let count = 0;
+
+  borders.forEach(border => {
+    border.forEach(coord => {
+      latSum += coord.lat;
+      lngSum += coord.lng;
+      count++;
+    });
+  });
+
+  return {
+    lat: latSum / count,
+    lng: lngSum / count,
+  };
+}
+
+function App() {
+  // border path for a Polyline
+  const borders = [
+    [
+      { lat: 43.6817, lng: -79.4572 },
+      { lat: 43.6329, lng: -79.4250 },
+      { lat: 43.6396, lng: -79.3935 },
+      { lat: 43.6879, lng: -79.3935 },
+      { lat: 43.6879, lng: -79.4572 },
+      { lat: 43.6817, lng: -79.4572 },
+    ]
+  ];
+
+  // center of the map based on the borders
+  const location = calculateCenter(borders);
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Home route */}
+        <Route path="/maps" element={<MapComponent location={location} borders={borders} />} /> {}
+      </Routes>
+    </>
   );
 }
 
--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -2,29 +2,50 @@
 
 import React, { useEffect, useRef } from 'react';
 
-const MapComponent = ({ location }) => {
+const MapComponent = ({ location, borders }) => {
   const mapRef = useRef(null);
 
   useEffect(() => {
-    const initMap = () => {
+    window.initMap = () => {
       const mapOptions = {
-        zoom: 14,
+        zoom: 13.4,
         center: location,
       };
       const map = new window.google.maps.Map(mapRef.current, mapOptions);
+
       new window.google.maps.Marker({
         position: location,
         map: map,
       });
-    };
-    if (window.google && window.google.maps) {
-      initMap();
-    } else {
-      window.initMap = initMap;
-    }
-  }, [location]);
 
-  return <div ref={mapRef} style={{ height: '400px', width: '100%' }} />;
+      // borders on the map and shade the inside area
+      borders.forEach(borderPath => {
+        const borderCoordinates = borderPath.map(coord => new window.google.maps.LatLng(coord.lat, coord.lng));
+        const shadedArea = new window.google.maps.Polygon({
+          paths: borderCoordinates,
+          strokeColor: '#FF0000',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: '#FF0000',
+          fillOpacity: 0.04, // make the shade lighter or darker
+        });
+        shadedArea.setMap(map);
+      });
+    };
+
+    if (!window.google || !window.google.maps) {
+      const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
+      script.async = true;
+      script.defer = true;
+      document.head.appendChild(script);
+    } else {
+      window.initMap();
+    }
+  }, [location, borders]);
+
+  return <div ref={mapRef} style={{ height: '87vh', width: '100%' }} />;
 };
 
 export default MapComponent;
