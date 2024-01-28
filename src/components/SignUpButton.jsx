0c4331ffb91fc9a4ac7bddfa3136431a8759f0bb
linkwithlionhart <linkwithlionhart@gmail.com>
2024-01-28 14:51:46 -0800

    implement null register button  when user logs in

--- src/components/SignUpButton.jsx
+++ src/components/SignUpButton.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const SignUpButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
 
-  return (
+  return !isAuthenticated ? (
     <button
       className='bg-transparent hover:bg-[#7D67AC] hover:text-white py-2 px-4 mr-2 border border-[#5b588a] hover:border-transparent rounded'
       onClick={() => loginWithRedirect({ screen_hint: 'signup' })}
     >
       Register
     </button>
-  );
+  ) : null;
 };
 
 export default SignUpButton;
