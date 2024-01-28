4ca5573bc686d1f754bed494f8eb6fbedd750362
linkwithlionhart <linkwithlionhart@gmail.com>
2024-01-28 15:48:39 -0800

    adjust SignUpButton component

--- src/components/SignUpButton.jsx
+++ src/components/SignUpButton.jsx
@@ -4,10 +4,14 @@ import { useAuth0 } from '@auth0/auth0-react';
 const SignUpButton = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
 
+  const handleSignUp = () => {
+    loginWithRedirect({ screen_hint: 'signup' });
+  };
+
   return !isAuthenticated ? (
     <button
       className='bg-transparent hover:bg-[#7D67AC] hover:text-white py-2 px-4 mr-2 border border-[#5b588a] hover:border-transparent rounded'
-      onClick={() => loginWithRedirect({ screen_hint: 'signup' })}
+      onClick={handleSignUp}
     >
       Register
     </button>
