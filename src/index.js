9c9e6e3bb69cdabc0ed919cccb5c015a35809b78
linkwithlionhart <linkwithlionhart@gmail.com>
2024-01-28 10:18:17 -0800

    import Auth0Provider, wrap BrowserRouter, provide config

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 import './index.css';
 import App from './App';
@@ -12,13 +13,18 @@ import Job_Postings from "./components/Job_Postings";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <BrowserRouter>
-     <Navbar/>
-      <Routes> 
-        <Route path="/" element={<App/>} />
-        <Route path="/jobs" element={<Job_Postings/>} />
-      </Routes>
-    </BrowserRouter>
+    <Auth0Provider // Needs to wrap around BrowserRouter
+      domain="dev-f5mq00rx18si8svy.us.auth0.com" // Replace with your Auth0 domain
+      clientId="XJrEAsjVDcZ2tWhyaeOPsNC5okqv3rdG" // Replace with your Auth0 client ID
+      redirectUri={window.location.origin}>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<App/>} />
+          <Route path="/jobs" element={<Job_Postings/>} />
+        </Routes>
+      </BrowserRouter>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
