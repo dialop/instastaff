747a8c31fc80b4133ec014ec02acfc3f0faaea87
linkwithlionhart <linkwithlionhart@gmail.com>
2024-01-28 15:51:00 -0800

    strip component for feature development

--- src/components/SignUpButton.jsx
+++ src/components/SignUpButton.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
 
 const SignUpButton = () => {
-  const { loginWithRedirect, isAuthenticated } = useAuth0();
 
-  const handleSignUp = () => {
-    loginWithRedirect({ screen_hint: 'signup' });
-  };
-
-  return !isAuthenticated ? (
+  return (
     <button
       className='bg-transparent hover:bg-[#7D67AC] hover:text-white py-2 px-4 mr-2 border border-[#5b588a] hover:border-transparent rounded'
-      onClick={handleSignUp}
     >
       Register
     </button>
-  ) : null;
+  );
 };
 
 export default SignUpButton;
