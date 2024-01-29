9dfe737fabe3292ab57b5789ddbff65cf8a3edf3
Diana Ichmoukhametov <akhtyamovadiana@gmail.com>
2024-01-29 10:46:01 -0500

    Fix calendar router to display data from db

--- server/app.js
+++ server/app.js
@@ -16,47 +16,6 @@ const calendarRouter = require("./routes/calendar")
 const mapsRoutes = require('./routes/map');
 const apiJobs = require('./routes/api/api_jobs');
 
-
-
-// //from calendar route 
-  app.get("/calendar", (req, res) => {
- 
-    console.log("hello from calendar route");
-    //Edit user ID with $ in quary based on user cookies.
-    // const userId = req.params.user_id;
-    // console.log("Is it working?")
-
-    pool.query(
-      `
-    SELECT
-    job_postings.title AS occupation,
-    job_postings.date AS shift_date,
-    job_postings.start_time AS start_shift,
-    job_postings.duration AS duration,
-    job_postings.facility_name AS facility_name,
-    job_postings.facility_short_address AS address
-    FROM job_postings 
-    JOIN users
-    ON users.id = job_postings.user_id 
-    WHERE users.id = 1;
-    `
-    ).then(({ rows: calendar }) => {
-      console.log("row", calendar);
-      res.json(calendar);
-    })
-    .catch((error) => {
-      console.error("Error executing SQL query:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    });
- 
-  });
-
-
-//end
-
-
-
-
 // View engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
@@ -75,7 +34,8 @@ app.use('/api/jobs', apiJobs(pool))
 app.use('/api', mapsRoutes);
 app.use("/api", indexRouter); 
 
-// app.use('/calendar', calendarRouter);
+//Calendar Route
+app.use('/calendar', calendarRouter);
 
 // Serve the React application
 // app.get("*", (req, res) => {
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client"; 
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Navbar from "./components/Nav";
 
 const container = document.getElementById('root'); 
 const root = createRoot(container);
@@ -14,9 +11,7 @@ const root = createRoot(container);
 // Render the app inside the root
 root.render(
   <React.StrictMode>
-
     <App/>
-
   </React.StrictMode>
 );
 
