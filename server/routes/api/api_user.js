bbe03a3c3e840dd775cac211b2aacb94456358b9
linkwithlionhart <linkwithlionhart@gmail.com>
2024-01-29 15:34:34 -0800

    add GET router for user info

--- server/routes/api/api_user.js
+++ server/routes/api/api_user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (pool) => {
+  
+  // POST to insert new user.
   router.post('/users', async (req, res) => {
     const { email, first_name, last_name } = req.body;
 
@@ -17,6 +19,28 @@ module.exports = (pool) => {
       res.status(201).json(result.rows[0]);
     } catch (err) {
       console.error('Error handling user data:', err);
+      res.status(500).send('Server error');
+    }
+  });
+
+  // GET to fetch a user's profile.
+  router.get('/profile', async (req, res) => {
+    // Retrieve and decode the user's email from the Auth0 token.
+    const userEmail = req.user.email;
+
+    try {
+      const result = await pool.query(
+        'SELECT * FROM users WHERE email = $1',
+        [userEmail]
+      );
+
+      if (result.rows.length > 0) {
+        res.json(result.rows[0]);
+      } else {
+        res.status(404).send('User not found');
+      }
+    } catch (error) {
+      console.error('Error fetching user from the database:', error);
       res.status(500).send('Server error');
     }
   });
