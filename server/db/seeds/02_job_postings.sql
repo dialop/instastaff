80c963c8881db6f04afa8a8a87477c1f10be8811
dialop <dianalop1998@gmail.com>
2024-01-29 21:39:04 -0500

    :safety_vest: DB JOBS
    Add coordinates for db job postings

--- server/db/seeds/02_job_postings.sql
+++ server/db/seeds/02_job_postings.sql
@@ -1,7 +1,7 @@
 INSERT INTO job_postings (user_id, feedback_id, title, type_of_worker, rate, gender, duration, date, start_time, facility_name, facility_short_address, facility_latitude, facility_longitude, available_to_choose, is_filled) VALUES
-(1, NULL, 'Registered Nurse', 'Full-time', 50, 'Any', 9, '2024-02-01', '09:00:00', 'Elysium Medical Center', '123 Tech St', 40.7128, -74.0060, TRUE, FALSE),
-(2, NULL, 'Personal Support Worker', 'Part-time', 33, 'Female', 12, '2024-02-15', '10:00:00', 'Aurelia Medical Group', '456 Design Ave', 37.7749, -122.4194, TRUE, FALSE),
-(3, NULL, 'Registered Nurse', 'Full-time', 65, 'Male', 8, '2024-03-01', '08:30:00', 'Veritas Health Systems', '202 Analyze Ave', 41.8781, -87.6298, TRUE, FALSE),
-(4, NULL, 'Personal Support Worker', 'Full-time', 35, 'Any', 12, '2024-02-15', '09:30:00', 'Aurelia Medical Group', '456 Design Ave', 37.7749, -122.4194, TRUE, FALSE),
-(5, NULL, 'Registered Nurse', 'Part-time', 60, 'Any', 8, '2024-03-01', '11:00:00', 'Elysium Medical Center', '123 Tech St', 40.7128, -74.0060, TRUE, FALSE),
-(6, NULL, 'Personal Support Worker', 'Full-time', 40, 'Any', 12, '2024-03-15', '08:00:00', 'Veritas Health Systems', '202 Analyze Ave', 41.8781, -87.6298, TRUE, FALSE);
+(1, NULL, 'Registered Nurse', 'Full-time', 50, 'Any', 9, '2024-02-01', '09:00:00', 'Elysium Medical Center', '123 Tech St', 43.6817, -79.4572, TRUE, FALSE),
+(2, NULL, 'Personal Support Worker', 'Part-time', 33, 'Female', 12, '2024-02-15', '10:00:00', 'Aurelia Medical Group', '456 Design Ave', 43.6329, -79.4250, TRUE, FALSE),
+(3, NULL, 'Registered Nurse', 'Full-time', 65, 'Male', 8, '2024-03-01', '08:30:00', 'Veritas Health Systems', '202 Analyze Ave', 43.6396, -79.3935, TRUE, FALSE),
+(4, NULL, 'Personal Support Worker', 'Full-time', 35, 'Any', 12, '2024-02-15', '09:30:00', 'Aurelia Medical Group', '456 Design Ave', 43.6879, -79.3935, TRUE, FALSE),
+(5, NULL, 'Registered Nurse', 'Part-time', 60, 'Any', 8, '2024-03-01', '11:00:00', 'Elysium Medical Center', '123 Tech St', 43.6879, -79.4572, TRUE, FALSE),
+(6, NULL, 'Personal Support Worker', 'Full-time', 40, 'Any', 12, '2024-03-15', '08:00:00', 'Veritas Health Systems', '202 Analyze Ave', 43.6817, -79.4572, TRUE, FALSE);
