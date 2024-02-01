fe566079d27ca549b8b2c295c56ee017f83a1f38
Diana Ichmoukhametov <akhtyamovadiana@gmail.com>
2024-01-31 23:21:53 -0500

    Add push notifications

--- src/hooks/useNotifications.js
+++ src/hooks/useNotifications.js
@@ -7,8 +7,29 @@ const useNotifications = (job) => {
     try {
       const response = await axios.post('/api/send-email', job);
       console.log(response.data);
+      toast.success('Shift booked successfully!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
     } catch (error) {
-      console.error("Error accepting job:", error);
+      console.error("Error booking job:", error);
+
+      toast.error('Error booking shift. Please try again.', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
     }
   };
 
