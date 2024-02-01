732ba7174c3a8a62a3b1c5d6e45538fd8aa949d0
Diana Ichmoukhametov <akhtyamovadiana@gmail.com>
2024-01-31 20:51:50 -0500

    Move email logic to hook

--- server/db/seeds/02_job_postings.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO job_postings (user_id, feedback_id, title, type_of_worker, rate, gender, duration, date, start_time, facility_name, facility_short_address, facility_latitude, facility_longitude, available_to_choose, is_filled) VALUES
-(1, NULL, 'Registered Nurse', 'Full-time', 90, 'Any', 9, '2024-02-01', '09:00:00', 'Elysium Medical Center', '123 Tech St', 43.6817, -79.4572, TRUE, FALSE),
-(2, NULL, 'Personal Support Worker', 'Part-time', 33, 'Female', 12, '2024-02-15', '10:00:00', 'Aurelia Medical Group', '456 Design Ave', 43.6329, -79.4250, TRUE, FALSE),
-(3, NULL, 'Personal Support Worker', 'Full-time', 35, 'Any', 12, '2024-02-15', '09:30:00', 'NeuraMed Nexus', '777 Willow Court', 43.6882, -79.3925, TRUE, FALSE),
-(4, NULL, 'Registered Nurse', 'Full-time', 85, 'Male', 8, '2024-03-01', '08:30:00', 'Veritas Health Systems', '202 Analyze Ave', 43.6396, -79.3935, TRUE, FALSE),
-(5, NULL, 'Registered Nurse', 'Part-time', 77, 'Any', 8, '2024-03-01', '11:00:00', 'NeuraCore LifeCare', '123 Birch St', 43.6879, -79.4570, TRUE, FALSE),
-(6, NULL, 'Personal Support Worker', 'Full-time', 40, 'Any', 12, '2024-03-15', '08:00:00', 'NeuraSphere Innovare', '44 Elmwood Avenue', 43.6827, -79.4532, TRUE, FALSE),
-(7, NULL, 'Registered Nurse', 'Part-time', 75, 'Female', 10, '2024-03-07', '07:45:00', 'NeuraMed Nexus', '777 Liddlow Drive', 43.6833, -79.4564, TRUE, FALSE),
-(8, NULL, 'Personal Support Worker', 'Part-time', 31, 'Female', 11, '2024-03-03', '09:00:00', 'RegenXcel MediCare', '789 BioTech Boulevard', 43.6859, -79.4522, TRUE, FALSE);
--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-let express = require("express");
-let router = express.Router();
-
-/* GET /api/message */
-router.get("/message", function (req, res, next) {
-  res.json("Hello from the API!");
-});
-
-module.exports = router;
