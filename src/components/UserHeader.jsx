b6b45ebb8fae925d35ada90089cc992073a57394
linkwithlionhart <linkwithlionhart@gmail.com>
2024-02-03 14:27:18 -0800

    implement auth0 linked user header

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography } from '@mui/material';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const UserHeader = () => {
+const { user } = useAuth0();
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          User Profile
+    <AppBar position="static" sx={{ bgcolor: '#6547A5' }}>
+      <Toolbar sx= {{ justifyContent: 'center'}}>
+        <Typography variant="h7" component="div">
+          {user ? `Hello, ${user.given_name}! Welcome back.` : 'Hello, InstaStaffer! Please login or register.'}
         </Typography>
       </Toolbar>
     </AppBar>
