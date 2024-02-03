551f01d58e8791893cb9edc60d61ab230e3363d1
dialop <dianalop1998>
2024-02-03 17:21:04 -0500

    :seedling: Update job_posting seeds
    Update job_posting seed coordinates

--- server/db/seeds/02_job_postings.sql
+++ server/db/seeds/02_job_postings.sql
@@ -1,9 +1,9 @@
 INSERT INTO job_postings (user_id, feedback_id, title, type_of_worker, rate, gender, duration, date, start_time, facility_name, facility_short_address, facility_latitude, facility_longitude, available_to_choose, is_filled) VALUES
-(1, NULL, 'Registered Nurse', 'Full-time', 90, 'Any', 9, '2024-02-01', '09:00:00', 'Elysium Medical Center', '123 Tech St', 43.6753, -79.4491, TRUE, FALSE),
-(2, NULL, 'Personal Support Worker', 'Part-time', 33, 'Female', 12, '2024-02-15', '10:00:00', 'Aurelia Medical Group', '456 Design Ave', 43.6632, -79.4112, TRUE, FALSE),
-(3, NULL, 'Personal Support Worker', 'Full-time', 35, 'Any', 12, '2024-02-15', '09:30:00', 'NeuraMed Nexus', '777 Willow Court', 43.6467, -79.4169, TRUE, FALSE),
-(4, NULL, 'Registered Nurse', 'Full-time', 85, 'Male', 8, '2024-03-01', '08:30:00', 'Veritas Health Systems', '202 Analyze Ave', 43.6518, -79.4326, TRUE, FALSE),
-(5, NULL, 'Registered Nurse', 'Part-time', 77, 'Any', 8, '2024-03-01', '11:00:00', 'NeuraCore LifeCare', '123 Birch St', 43.6805, -79.4239, TRUE, FALSE),
-(6, NULL, 'Personal Support Worker', 'Full-time', 40, 'Any', 12, '2024-03-15', '08:00:00', 'NeuraSphere Innovare', '44 Elmwood Avenue', 43.6489, -79.4078, TRUE, FALSE),
-(7, NULL, 'Registered Nurse', 'Part-time', 75, 'Female', 10, '2024-03-07', '07:45:00', 'NeuraMed Nexus', '777 Liddlow Drive', 43.6544, -79.4318, TRUE, FALSE),
-(8, NULL, 'Personal Support Worker', 'Part-time', 31, 'Female', 11, '2024-03-03', '09:00:00', 'RegenXcel MediCare', '789 BioTech Boulevard', 43.6772, -79.4437, TRUE, FALSE);
+(1, NULL, 'Registered Nurse', 'Full-time', 90, 'Any', 9, '2024-02-01', '09:00:00', 'Elysium Medical Center', '123 Tech St', 43.6641, -79.4205, TRUE, FALSE), -- Near Leeds St and Roblocke Ave, Toronto
+(2, NULL, 'Personal Support Worker', 'Part-time', 33, 'Female', 12, '2024-02-15', '10:00:00', 'Aurelia Medical Group', '456 Design Ave', 43.6703, -79.4072, TRUE, FALSE), -- Near The Annex, Toronto
+(3, NULL, 'Personal Support Worker', 'Full-time', 35, 'Any', 12, '2024-02-15', '09:30:00', 'NeuraMed Nexus', '777 Willow Court', 43.6787, -79.4095, TRUE, FALSE), -- Near Tarragon Village, Toronto
+(4, NULL, 'Registered Nurse', 'Full-time', 85, 'Male', 8, '2024-03-01', '08:30:00', 'Veritas Health Systems', '202 Analyze Ave', 43.6655, -79.4376, TRUE, FALSE), -- Near Dovercourt Village, Toronto
+(5, NULL, 'Registered Nurse', 'Part-time', 77, 'Any', 8, '2024-03-01', '11:00:00', 'NeuraCore LifeCare', '123 Birch St', 43.6783, -79.4429, TRUE, FALSE), -- Near Regal Heights, Toronto
+(6, NULL, 'Personal Support Worker', 'Full-time', 40, 'Any', 12, '2024-03-15', '08:00:00', 'NeuraSphere Innovare', '44 Elmwood Avenue', 43.6555, -79.4313, TRUE, FALSE), -- Near Via Italia and Rosemount Ave, Toronto
+(7, NULL, 'Registered Nurse', 'Part-time', 75, 'Female', 10, '2024-03-07', '07:45:00', 'NeuraMed Nexus', 'Bathurst St & Adelaide St W', 43.6477, -79.4002, TRUE, FALSE), -- Near Bathurst St and Adelaide St W, Toronto
+(8, NULL, 'Personal Support Worker', 'Part-time', 31, 'Female', 11, '2024-03-03', '09:00:00', 'RegenXcel MediCare', 'Palmerston Ave & London St', 43.6530, -79.4089, TRUE, FALSE); -- Near Palmerston Ave and London St, Toronto
