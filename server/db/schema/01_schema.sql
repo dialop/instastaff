59dd577e4180dd4a14d1544c9454e8f6d1b4610f
linkwithlionhart <linkwithlionhart@gmail.com>
2024-02-04 11:50:18 -0800

    adjust user schema

--- server/db/schema/01_schema.sql
+++ server/db/schema/01_schema.sql
@@ -7,17 +7,18 @@ DROP TABLE IF EXISTS users CASCADE;
 -- USERS TABLE
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
+  auth0_id VARCHAR(50),
   first_name VARCHAR(255),
   last_name VARCHAR(255),
-  handle VARCHAR(255),
   email VARCHAR(255) NOT NULL,
+  handle VARCHAR(255),
   password VARCHAR(255),
   profile_picture VARCHAR(255),
   gender VARCHAR(50),
   occupation VARCHAR(255),
   license VARCHAR(255),
+  isHero BOOLEAN,
   points INTEGER DEFAULT 0,
-  badge_id INTEGER,
   last_login TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   token VARCHAR(255)
 );
