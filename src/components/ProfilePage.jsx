184c172e0ce4ee94a732b640a7ef9af31627fa52
linkwithlionhart <linkwithlionhart@gmail.com>
2024-02-03 21:18:36 -0800

    clean and refactor for debugging

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,111 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { Typography, Box, Grid, Card, CardContent, Avatar, Button, IconButton, Paper, Badge, TextField } from '@mui/material';
-import { blue } from '@mui/material/colors';
-import { styled } from '@mui/material/styles';
-import CameraAltIcon from '@mui/icons-material/CameraAlt';
-import EditIcon from '@mui/icons-material/Edit'; 
+import React from 'react';
+import { Typography, Box, Grid, Card, CardContent } from '@mui/material';
 import BarChartComponent from './profile/BarChartComponent';
 import Rewards from './profile/rewards/Rewards';
 import GiftCard from './profile/rewards/GiftCard'; 
 import PieChartInsentive from './profile/PieChartInsentive';
 import RegistrationForm from './profile/RegistrationForm';
 
-// Define a styled Avatar component
-const SmallAvatar = styled(Avatar)(({ theme }) => ({
-  width: 30,
-  height: 30,
-  border: `2px solid ${theme.palette.background.paper}`,
-}));
-
 function ProfilePage() {
-  // Define state to store API data
-  const [apiData, setApiData] = useState([]);
-  const [isEditMode, setIsEditMode] = useState(false); 
-  const [avatar, setAvatar] = useState(localStorage.getItem('userAvatar') || '');
-  const [badgeAvatar, setBadgeAvatar] = useState(localStorage.getItem('badgeAvatar') || '/static/images/avatar/1.jpg');
-  const defaultAvatar = '/path/to/default/avatar.jpg';
-
-  useEffect(() => {
-    const storedAvatar = localStorage.getItem('userAvatar');
-    const storedBadgeAvatar = localStorage.getItem('badgeAvatar');
-    if (storedAvatar) {
-      setAvatar(storedAvatar);
-    }
-    if (storedBadgeAvatar) {
-      setBadgeAvatar(storedBadgeAvatar);
-    }
-
-    // API request when the component mounts
-    fetch('s', {
-      headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_CARDLY}`,
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setApiData(data);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
-
-  const handleAvatarChange = (event) => {
-    if (event.target.files && event.target.files[0]) {
-      const newAvatar = URL.createObjectURL(event.target.files[0]);
-      setAvatar(newAvatar);
-      localStorage.setItem('userAvatar', newAvatar);
-    }
-  };
-
-  const handleBadgeAvatarChange = (event) => {
-    if (event.target.files && event.target.files[0]) {
-      const newBadgeAvatar = URL.createObjectURL(event.target.files[0]);
-      setBadgeAvatar(newBadgeAvatar);
-      localStorage.setItem('badgeAvatar', newBadgeAvatar);
-    }
-  };
-
-  const [userData, setUserData] = useState({
-    name: 'Diana Lopez',
-    title: 'Registered Nurse, RN',
-    location: 'Toronto, ON',
-    avatar: avatar || defaultAvatar,
-  });
-
-  const handleEditClick = () => {
-    setIsEditMode(true);
-  };
-
-  const handleSaveClick = () => {
-    setIsEditMode(false);
-    
-    localStorage.setItem('userName', userData.name);
-  };
-
-  const handleNameChange = (event) => {
-    const newName = event.target.value;
-    setUserData({
-      ...userData,
-      name: newName,
-    });
-  };
-
-  const handleTitleChange = (event) => {
-    const newTitle = event.target.value;
-    setUserData({
-      ...userData,
-      title: newTitle,
-    });
-  };
-
-  const handleLocationChange = (event) => {
-    const newLocation = event.target.value;
-    setUserData({
-      ...userData,
-      location: newLocation,
-    });
-  };
 
   return (
     <Box m={2}>
@@ -115,114 +16,8 @@ function ProfilePage() {
         <RegistrationForm />
       </Box>
 
-      {/* Profile Card */}
+      {/* Information Section */}
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={13}>
-          <Card sx={{ textAlign: 'center', p: 2 }}>
-            <input
-              accept="image/*"
-              style={{ display: 'none' }}
-              id="avatar-upload"
-              type="file"
-              onChange={handleAvatarChange}
-            />
-            <input
-              accept="image/*"
-              style={{ display: 'none' }}
-              id="badge-avatar-upload"
-              type="file"
-              onChange={handleBadgeAvatarChange}
-            />
-            <label htmlFor="avatar-upload">
-              <IconButton color="primary" component="span" sx={{ position: 'relative' }}>
-                <Badge
-                  overlap="circular"
-                  anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                  badgeContent={
-                    <SmallAvatar
-                      alt="Badge Avatar"
-                      src={badgeAvatar}
-                      onClick={() => document.getElementById('badge-avatar-upload').click()}
-                    />
-                  }
-                >
-                  <Avatar
-                    alt={userData.name}
-                    src={userData.avatar}
-                    sx={{ width: 100, height: 100, margin: 'auto', bgcolor: blue[500] }}
-                  />
-                </Badge>
-                {!avatar && (
-                  <CameraAltIcon sx={{ position: 'absolute', bottom: '10px', right: '10px', fontSize: 'large', color: 'white' }} />
-                )}
-              </IconButton>
-            </label>
-            {isEditMode ? (
-              <TextField
-                fullWidth
-                label="Name"
-                variant="outlined"
-                value={userData.name}
-                onChange={handleNameChange}
-              />
-            ) : (
-              <div>
-                <Typography variant="h5" sx={{ mt: 2 }}>
-                  {userData.name}
-                  {isEditMode ? null : (
-                    <IconButton
-                      color="primary"
-                      aria-label="Edit Name"
-                      component="span"
-                      onClick={handleEditClick}
-                    >
-                      <EditIcon />
-                    </IconButton>
-                  )}
-                </Typography>
-              </div>
-            )}
-            {isEditMode ? (
-              <TextField
-                fullWidth
-                label="Designation"
-                variant="outlined"
-                value={userData.title}
-                onChange={handleTitleChange}
-              />
-            ) : (
-              <Typography color="textSecondary">{userData.title}</Typography>
-            )}
-            {isEditMode ? (
-              <TextField
-                fullWidth
-                label="Location"
-                variant="outlined"
-                value={userData.location}
-                onChange={handleLocationChange}
-              />
-            ) : (
-              <Typography color="textSecondary" sx={{ mb: 2 }}>
-                {userData.location}
-              </Typography>
-            )}
-            {isEditMode ? (
-              <Button variant="contained" color="primary" onClick={handleSaveClick}>
-                Save
-              </Button>
-            ) : (
-              <Button variant="contained" onClick={handleEditClick}>
-                Edit
-              </Button>
-            )}
-            {/* Placeholder for social links */}
-            <Box sx={{ mt: 2 }}>
-              {/* Render social links here */}
-            </Box>
-          </Card>
-        </Grid>
-
-        {/* Information Section */}
         <Grid item xs={12} sm={6} md={8}>
           <Card>
             <CardContent>
@@ -247,15 +42,11 @@ function ProfilePage() {
         {/* Graph and Pie Chart Sections */}
         <Grid item xs={12} md={6}>
           <Typography variant="h6">Total Hours Summary</Typography>
-          <Paper>
-            <BarChartComponent />
-          </Paper>
+          <BarChartComponent />
         </Grid>
         <Grid item xs={12} md={6}>
           <Typography variant="h6">Health/Facility Summary</Typography>
-          <Paper>
-            <PieChartInsentive />
-          </Paper>
+          <PieChartInsentive />
         </Grid>
       </Grid>
     </Box>
