199cde3b7995558d380a020a07850fe2b98f8fdc
Diana Ichmoukhametov <akhtyamovadiana@gmail.com>
2024-02-04 23:04:50 -0500

    Update styling

--- src/components/AdminPostShift.jsx
+++ src/components/AdminPostShift.jsx
@@ -1,31 +1,62 @@
-import React, { useState } from 'react';
-import { Container, Button, Checkbox, FormControl, Radio, RadioGroup, FormLabel, Grid, MenuItem, Select, TextField, FormGroup, FormControlLabel } from '@mui/material';
+
+import React, { useState, useEffect } from 'react';
+import { Container, Button, FormControl, FormControlLabel, Radio, RadioGroup, FormLabel, Grid, MenuItem, Select, TextField } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const Input = styled(TextField)({
+  '& label.Mui-focused': {
+    color: '#6547A5',
+  },
+  '& .MuiOutlinedInput-root': {
+    '&.Mui-focused fieldset': {
+      borderColor: '#6547A5',
+    },
+  },
+});
+
 
 const PostShiftForm = () => {
-  const [facility, setFacility] = useState('');
-  const [address, setAddress] = useState('');
-  const [workerType, setWorkerType] = useState(''); 
-  // const [nurseChecked, setNurseChecked] = useState(false);
-  // const [pswChecked, setPswChecked] = useState(false);
-  const [rate, setRate] = useState('');
-  const [gender, setGender] = useState('');
-  const [duration, setDuration] = useState('');
-  const [startDate, setStartDate] = useState('');
-  const [startTime, setStartTime] = useState('');
+
+  
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
 
-  const handleFacilityChange = (event) => {
-    setFacility(event.target.value);
+  const [formData, setFormData] = useState({
+    facility: '',
+    address: '',
+    workerType: '',
+    rate: '',
+    gender: '',
+    duration: '',
+    startDate: '',
+    startTime: '',
+    latitude,
+    longitude
+  });
+
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
   };
 
-  const handleAddressChange = async (event) => {
-    setAddress(event.target.value);
+
+    useEffect(() => {
+   
+    console.log(formData);
+  }, [formData]); 
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
     try {
       const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
       const response = await fetch(
         `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-          address
+          formData.address
         )}&key=${apiKey}`
       );
 
@@ -35,6 +66,12 @@ const PostShiftForm = () => {
         const location = data.results[0].geometry.location;
         setLatitude(location.lat);
         setLongitude(location.lng);
+
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          latitude: location.lat,
+          longitude: location.lng,
+        }));
       }
     } catch (error) {
       console.error('Error fetching geocode data', error);
@@ -42,179 +79,122 @@ const PostShiftForm = () => {
 
   };
 
-  // const handleNurseChange = (event) => {
-  //   setNurseChecked(event.target.checked);
-  // };
-
-  // const handlePswChange = (event) => {
-  //   setPswChecked(event.target.checked);
-  // };
-
-  const handleWorkerTypeChange = (event) => {
-    setWorkerType(event.target.value);
-  };
-
-  const handleRateChange = (event) => {
-    setRate(event.target.value);
-  };
-
-  const handleGenderChange = (event) => {
-    setGender(event.target.value);
-  };
-
-  const handleDurationChange = (event) => {
-    setDuration(event.target.value);
-  };
-
-  const handleStartDateChange = (event) => {
-    setStartDate(event.target.value);
-  };
-
-  const handleStartTimeChange =  (event) => {
-    setStartTime(event.target.value);
-  };
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-
-
-    console.log({
-      facility,
-      address,
-      // nurseChecked,
-      // pswChecked,
-      rate,
-      gender,
-      duration,
-      startDate,
-      startTime,
-      latitude,
-      longitude
-    });
-  };
-
   return (
-    <Container maxWidth="sm" >
+    <>
       <h1 className="text-6xl p-8 text-[#24233E] text-center">Post a Shift</h1>
-      <form onSubmit={handleFormSubmit} style={{ border: '1px solid #ccc', borderRadius: '5px', padding: '16px' }}>
-        <FormControl fullWidth style={{ marginBottom: '20px' }}>
-          <TextField
-            value={facility}
-            onChange={handleFacilityChange}
-            size='small'
+      <Container maxWidth="sm" className="bg-white p-6 rounded-lg shadow-md mt-5" >
+
+        <form onSubmit={handleFormSubmit} className="space-y-4" >
+          <Input fullWidth
             label="Facility Name"
+            name="facility"
+            value={formData.facility}
+            onChange={handleInputChange}
           />
-        </FormControl>
 
-        <FormControl fullWidth style={{ marginBottom: '20px' }}>
-          <TextField
+          <Input fullWidth
             label="Facility Address"
-            value={address}
-            size='small'
-            onChange={handleAddressChange}
+            name="address"
+            value={formData.address}
+            onChange={handleInputChange}
           />
-        </FormControl>
 
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6}>
-            <FormControl component="fieldset" fullWidth style={{ marginBottom: '20px' }}>
-              <FormLabel component="legend">Type of Worker</FormLabel>
-              <RadioGroup
-                row
-                aria-label="workerType"
-                name="workerType"
-                value={workerType}
-                onChange={handleWorkerTypeChange}
-              >
-                <FormControlLabel
-                  value="nurse"
-                  control={<Radio />}
-                  label="Registered Nurse"
-                />
-                <FormControlLabel
-                  value="psw"
-                  control={<Radio />}
-                  label="Personal Support Worker"
-                />
-              </RadioGroup>
-            </FormControl>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>
+              <FormControl component="fieldset" fullWidth >
+                <FormLabel component="legend">Type of Worker</FormLabel>
+                <RadioGroup
+                  row
+                  aria-label="workerType"
+                  name="workerType"
+                  value={formData.workerType}
+                  onChange={handleInputChange}
+                >
+                  <FormControlLabel
+                    value="nurse"
+                    control={<Radio />}
+                    label="Registered Nurse"
+                  />
+                  <FormControlLabel
+                    value="psw"
+                    control={<Radio />}
+                    label="Personal Support Worker"
+                  />
+                </RadioGroup>
+              </FormControl>
+            </Grid>
           </Grid>
-        </Grid>
 
-        <FormControl fullWidth style={{ marginBottom: '20px' }}>
-          <FormLabel component="legend">Gender</FormLabel>
-          <Select
-            value={gender}
-            onChange={handleGenderChange}
-            size='small'
-          >
-            <MenuItem value="any">Any</MenuItem>
-            <MenuItem value="male">Male</MenuItem>
-            <MenuItem value="female">Female</MenuItem>
-          </Select>
-        </FormControl>
+          <FormControl fullWidth >
+            <FormLabel component="legend">Gender</FormLabel>
+            <Select
+              value={formData.gender}
+              onChange={handleInputChange}
+              name="gender"
+            >
+              <MenuItem value="any">Any</MenuItem>
+              <MenuItem value="male">Male</MenuItem>
+              <MenuItem value="female">Female</MenuItem>
+            </Select>
+          </FormControl>
 
-
-        <Grid container spacing={2} style={{ marginBottom: '20px' }}>
-          <Grid item xs={6}>
-            <TextField
-              label="Rate"
-              type="number"
-              fullWidth
-              value={rate}
-              size='small'
-              onChange={handleRateChange}
-            />
+          <Grid container spacing={3} >
+            <Grid item xs={6}>
+              <Input fullWidth
+                label="Rate"
+                type="number"
+                name="rate"
+                value={formData.rate}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <Input fullWidth
+                label="Shift duration"
+                type="number"
+                name="duration"
+                value={formData.duration}
+                onChange={handleInputChange}
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={6}>
-            <TextField
-              label="Duration"
-              type="number"
-              fullWidth
-              value={duration}
-              size='small'
-              onChange={handleDurationChange}
-            />
-          </Grid>
-        </Grid>
 
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <TextField
-              label="Start Date"
-              type="date"
-              fullWidth
-              value={startDate}
-              onChange={handleStartDateChange}
-              size='small'
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
+          <Grid container spacing={3}>
+            <Grid item xs={6}>
+              <Input fullWidth
+                label="Start Date"
+                type="date"
+                name="startDate"
+                value={formData.startDate}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <Input fullWidth
+                label="Start Time"
+                type="time"
+                name="startTime"
+                value={formData.startTime}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                onChange={handleInputChange}
+                inputProps={{
+                  step: 300,
+                }}
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={6}>
-            <TextField
-              label="Start Time"
-              type="time"
-              fullWidth
-              value={startTime}
-              onChange={handleStartTimeChange}
-              size='small'
-              InputLabelProps={{
-                shrink: true,
-              }}
-              inputProps={{
-                step: 300,
-              }}
-            />
-          </Grid>
-        </Grid>
 
-        <Button type="submit" variant="contained" color="primary">
-          Post Job
-        </Button>
-      </form>
-    </Container>
+          <Button type="submit" fullWidth variant="contained" style={{ backgroundColor: '#6547A5', color: 'white' }}>
+            Post a Shift
+          </Button>
+        </form>
+      </Container>
+    </>
   );
 };
 
