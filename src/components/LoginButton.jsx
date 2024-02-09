3f40c026eea34886353bec939c6b64fb91e14d37
linkwithlionhart <linkwithlionhart@gmail.com>
2024-02-09 00:11:41 -0800

    redirect on login to /profile

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
@@ -15,7 +14,7 @@ const LoginButton = () => {
       {!isAuthenticated && (
         <button
           className='bg-[#6547A5] hover:bg-[#7D67AC] text-white py-2.5 px-4 border-[#24233E] hover:border-transparent rounded'
-          onClick={() => loginWithRedirect()}
+          onClick={() => loginWithRedirect({redirectUri: 'http://localhost:3001/profile'})}
         >
           Login
         </button>
