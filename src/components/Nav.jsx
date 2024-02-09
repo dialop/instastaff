c1baaf3bed8e44ebb503c3d6c5b10255609467f3
linkwithlionhart <linkwithlionhart@gmail.com>
2024-02-09 00:11:06 -0800

    clean and refactor Nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,13 +9,11 @@ import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from './LoginButton';
 import SignUpButton from './SignUpButton';
 
-
 const Navbar = ({ isAdmin }) => {
   const [nav, setNav] = useState(false);
   const [userData, setUserData] = useState(null);
   const location = useLocation();
   const { isAuthenticated, user } = useAuth0();
-
 
   useEffect(() => {
     if (isAuthenticated) {
@@ -23,14 +21,11 @@ const Navbar = ({ isAdmin }) => {
       console.log('from nav', userData);
     }
   }, [isAuthenticated, user]);
-  
-
 
   const handleNav = () => {
     setNav(!nav);
   };
   
-
   const isActive = (pathname) => location.pathname === pathname;
 
   return (
@@ -39,19 +34,18 @@ const Navbar = ({ isAdmin }) => {
         <Link to="/">InstaStaff</Link> 
       </h1>
       <ul className='hidden md:flex items-center'>
-      {userData && userData.is_admin && ( 
-
+        {userData && userData.is_admin && ( 
           <>
-      <li className={`p-4 ${isActive('/post-jobs') ? 'border-t-4 border-[#24233E]' : ''}`}>
-          <Link to="/post-shift">Post New Job</Link>
-        </li>
-        <li className={`p-4 ${isActive('/jobs') ? 'border-t-2 border-[#24233E]' : ''}`}>
-          <Link to="/jobs">Jobs</Link>
-        </li>
-        <LoginButton />
-        </>
+            <li className={`p-4 ${isActive('/post-jobs') ? 'border-t-4 border-[#24233E]' : ''}`}>
+              <Link to="/post-shift">Post New Job</Link>
+            </li>
+            <li className={`p-4 ${isActive('/jobs') ? 'border-t-2 border-[#24233E]' : ''}`}>
+              <Link to="/jobs">Jobs</Link>
+            </li>
+            <LoginButton />
+          </>
         )}
-              <li className={`p-4 ${isActive('/jobs') ? 'border-t-2 border-[#24233E]' : ''}`}>
+        <li className={`p-4 ${isActive('/jobs') ? 'border-t-2 border-[#24233E]' : ''}`}>
           <Link to="/jobs">Jobs</Link>
         </li>
         <li className={`p-4 ${isActive('/calendar') ? 'border-t-2 border-[#24233E]' : ''}`}>
@@ -70,18 +64,19 @@ const Navbar = ({ isAdmin }) => {
       <div onClick={handleNav} className='block md:hidden z-10'>
         {nav ? <IoMdClose size={30}/> : <GiHamburger size={30}/>}
       </div>
-      <ul className={nav ? 'fixed left-0 top-0 w-full h-full bg-[#6547A5] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-        <li className='p-6'><Link to="/jobs" className='flex justify-center items-center text-2xl' onClick={handleNav}>Jobs</Link></li>
-        <li className='p-6'><Link to="/calendar" className='flex justify-center items-center text-2xl' onClick={handleNav}>Calendar</Link></li>
-        <li className='p-6'><Link to="/maps" className='flex justify-center items-center text-2xl' onClick={handleNav}>Maps</Link></li>
-        <li className='p-6'><Link to="/profile" className='flex justify-center items-center text-2xl' onClick={handleNav}>Profile</Link></li>
-        <div className='flex flex-col items-center'>
-          <button className='bg-transparent hover:bg-[#7D67AC] hover:text-white py-2 px-4 mb-4 border border-[#5b588a] hover:border-transparent rounded'>Register</button>
-          <button className='bg-[#6547A5] hover:bg-[#7D67AC] text-white py-2.5 px-4 border-[#24233E] hover:border-transparent rounded'>Login</button>
-        </div>
-      </ul>
     </div>
   );
 };
 
 export default Navbar;
+
+{/* <ul className={nav ? 'fixed left-0 top-0 w-full h-full bg-[#6547A5] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+<li className='p-6'><Link to="/jobs" className='flex justify-center items-center text-2xl' onClick={handleNav}>Jobs</Link></li>
+<li className='p-6'><Link to="/calendar" className='flex justify-center items-center text-2xl' onClick={handleNav}>Calendar</Link></li>
+<li className='p-6'><Link to="/maps" className='flex justify-center items-center text-2xl' onClick={handleNav}>Maps</Link></li>
+<li className='p-6'><Link to="/profile" className='flex justify-center items-center text-2xl' onClick={handleNav}>Profile</Link></li>
+<div className='flex flex-col items-center'>
+  <button className='bg-transparent hover:bg-[#7D67AC] hover:text-white py-2 px-4 mb-4 border border-[#5b588a] hover:border-transparent rounded'>Register</button>
+  <button className='bg-[#6547A5] hover:bg-[#7D67AC] text-white py-2.5 px-4 border-[#24233E] hover:border-transparent rounded'>Login</button>
+</div>
+</ul> */}
