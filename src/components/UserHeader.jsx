12f552fcebbfe13c1566358bade5c8a17f72dbe2
linkwithlionhart <linkwithlionhart@gmail.com>
2024-02-09 10:27:42 -0800

    clean and refactor

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -19,26 +19,3 @@ const { user } = useAuth0();
 };
 
 export default UserHeader;
-
-// import React from 'react';
-// import { AppBar, Toolbar, Typography } from '@mui/material';
-// import { useAuth0 } from '@auth0/auth0-react';
-// import PointsProgress from './profile/rewards/PointsProgress';
-
-// // const UserHeader = () => {
-// //   const { user } = useAuth0();
-
-// //   return (
-// //     <AppBar position="static" sx={{ bgcolor: '#6547A5' }}>
-// //       <Toolbar sx={{ justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
-// //         <Typography variant="h6" component="div" sx={{ mt: 1 }}>
-// //           {user ? `Hello, ${user.given_name}! Welcome back.` : 'Hello, InstaStaffer! Please login or register.'}
-// //         </Typography>
-// //         {/* You may want to add a conditional rendering here if PointsProgress needs user info */}
-// //         <PointsProgress />
-// //       </Toolbar>
-// //     </AppBar>
-// //   );
-// // };
-
-// // export default UserHeader;
--- src/components/profile/rewards/PointsProgress.jsx
+++ src/components/profile/rewards/PointsProgress.jsx
@@ -49,30 +49,3 @@ const PointsProgress = () => {
 };
 
 export default PointsProgress;
-
-// const PointsProgress = () => {
-//   const [currentPoints] = useState(150);
-//   const totalPoints = 500;  
-//   const progressPercent = (currentPoints / totalPoints) * 100;
-
-//   return (
-//     <Box display="flex" flexDirection="column" alignItems="center" p={2}>
-//       <Box width="100%" mb={0} sx={{ borderRadius: 20, overflow: 'hidden' }}>
-//         <LinearProgress 
-//           variant="determinate" 
-//           value={progressPercent} 
-//           sx={{
-//             height: 10,
-//             borderRadius: 20,
-//             '& .MuiLinearProgress-bar': {
-//               backgroundColor: 'rgba(0, 255, 0, 0.75)'
-//             }
-//           }}
-//         />
-//       </Box>
-//       <Typography variant="subtitle1">
-//         {`${currentPoints}/${totalPoints} points to your next reward!`}
-//       </Typography>
-//     </Box>
-//   );
-// };
