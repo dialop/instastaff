7abae7416388c38bb9091232eae690e32509ba85
linkwithlionhart <linkwithlionhart@gmail.com>
2024-02-11 09:05:57 -0800

    account or edge case when no profile picture

--- src/components/user/ProfileAvatar.jsx
+++ src/components/user/ProfileAvatar.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Avatar } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const ProfileAvatar = () => {
   const { user, isAuthenticated } = useAuth0();
 
   if (!isAuthenticated) return null;
 
-  return (
-    <div className="flex items-center">
-      <Avatar alt={user.name} src={user.picture} sx={{ width: 48, height: 48 }} />
-    </div>
+  const avatarContent = user.picture ? (
+    <Avatar alt={user.name} src={user.picture} sx={{ width: 48, height: 48 }} />
+  ) : (
+    <Avatar sx={{ width: 48, height: 48 }}>
+      <AccountCircleIcon sx={{ color: "#C0ABF4" }} />
+    </Avatar>
   );
-};
 
+  return <div className="flex items-center mr-4">{avatarContent}</div>;
+};
 export default ProfileAvatar;
