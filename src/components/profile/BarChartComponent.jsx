ef349ac6e839de8a05b64f011a81a536f250ac3b
Diana Ichmoukhametov <akhtyamovadiana@gmail.com>
2024-02-12 23:01:20 -0500

    Fetch data for barChart

--- src/components/profile/BarChartComponent.jsx
+++ src/components/profile/BarChartComponent.jsx
@@ -1,25 +1,90 @@
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-const data = [
-  { name: 'Jan', Hours: 58 },
-  { name: 'Feb', Hours: 34 },
-  { name: 'Mar', Hours: 69}
+const BarChartComponent = () => {
+  const [calendarData, setCalendarData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const { user, isAuthenticated } = useAuth0();
+  const [aggregatedData, setAggregatedData] = useState([]);
   
-];
+  
 
-const BarChartComponent = () => (
-  <ResponsiveContainer width="100%" height={300}>
-    <BarChart data={data}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="Hours" fill="#8884d8" />
-    </BarChart>
-  </ResponsiveContainer>
-);
+  useEffect(() => {
+    const fetchCalendarData = async () => {
+      try {
+        if (isAuthenticated) {
+          setIsLoading(true);
+          const userId = window.sessionStorage.getItem('userId');
+          const response = await fetch(`/calendar/${userId}`);
+          if (!response.ok) {
+            throw new Error("Failed to fetch calendar data");
+          }
+          const data = await response.json();
+          setCalendarData(data);
+          setIsLoading(false);
+        }
+      } catch (error) {
+        setError(error);
+        console.error("Error fetching calendar data:", error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchCalendarData();
+  }, [isAuthenticated]);
+
+  
+  
+  useEffect(() => {
+    if (calendarData) {
+     
+      const aggregatedData = calendarData.reduce((acc, shift) => {
+        const shiftDate = new Date(shift.shift_date);
+        const month = shiftDate.getMonth(); 
+        const duration = shift.duration; 
+        const existingMonthDataIndex = acc.findIndex(item => item.month === month);
+        if (existingMonthDataIndex !== -1) {
+          
+          acc[existingMonthDataIndex].Hours += duration;
+        } else {
+          
+          acc.push({ month, Hours: duration });
+        }
+        return acc;
+      }, []);
+      setAggregatedData(aggregatedData);
+    }
+  }, [calendarData]);
+
+  const monthNames = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+
+  if (!calendarData || calendarData.length === 0) {
+  return <div>No hours available</div>;
+}
+
+  return (
+    <div>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>Error: {error.message}</div>}
+      {aggregatedData && (
+        <ResponsiveContainer width="100%" height={300}>
+          <BarChart data={aggregatedData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" tickFormatter={month => monthNames[month]} />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="Hours" fill="#8884d8" />
+          </BarChart>
+        </ResponsiveContainer>
+      )}
+    </div>
+  );
+};
 
 export default BarChartComponent;
