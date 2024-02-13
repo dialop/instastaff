6fbb3932decd015bc91f7cc617e62ff9e7bb15ea
linkwithlionhart <linkwithlionhart@gmail.com>
2024-02-12 21:30:37 -0800

    prepare for content

--- src/components/profile/RewardsBadges.jsx
+++ src/components/profile/RewardsBadges.jsx
@@ -13,28 +13,10 @@ const RewardsBadges = () => {
   }
 
   return (
-    <Container maxWidth="sm" className="bg-white p-6 rounded-lg shadow-md mt-5">
-      <CardContent>
+    <Container >
+      <CardContent maxWidth="sm" className="bg-white p-6 rounded-lg shadow-md mt-5">
         <Grid container spacing={2} alignItems="center">
-          <Box className="flex flex-col items-center w-full">
-            <Paper elevation={4} sx={{ width: '100%', mb: 2, borderRadius: 20, overflow: 'hidden' }}>
-              <LinearProgress 
-                variant="determinate" 
-                value={progressPercent} 
-                sx={{
-                  height: 10,
-                  borderRadius: 20,
-                  '& .MuiLinearProgress-bar': {
-                    backgroundColor: 'rgba(0, 255, 0, 0.75)'
-                  }
-                }}
-              />
-            </Paper>
-            <Typography variant="subtitle1" className="font-bold" sx={{ color: '#6547A5' }}>
-              {`${points}/${totalPoints} points to your next reward!`}
-            </Typography>
-            <Chip label={`${progressPercent}% Achieved`} color="primary" variant="outlined" sx={{ mt: 1 }} />
-          </Box>
+
         </Grid>
       </CardContent>
     </Container>
