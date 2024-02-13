aca6d1ee39dce9bf2b1f79a55092ca29baf1fe7b
linkwithlionhart <linkwithlionhart@gmail.com>
2024-02-12 19:38:52 -0800

    clean code

--- src/components/profile/ProfilePage.jsx
+++ src/components/profile/ProfilePage.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Typography, Box, Grid, Card, CardContent } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 
 import RegistrationForm from './RegistrationForm';
 import ProfileCard from './ProfileCard';
 import BarChartComponent from './BarChartComponent';
 import PieChartIncentive from './PieChartIncentive';
-// import Rewards from './profile/rewards/Rewards';
-// import GiftCard from './profile/rewards/GiftCard'; 
 
 function ProfilePage() {
 
@@ -42,24 +40,3 @@ function ProfilePage() {
 }
 
 export default ProfilePage;
-
-      // {/* Information Section */}
-      // <Grid container spacing={2}>
-      //   <Grid item xs={12} sm={6} md={8}>
-      //     <Card>
-      //       <CardContent>
-      //         <Typography variant="h5" gutterBottom>Rewards</Typography>
-      //         <form noValidate autoComplete="off">
-      //           <Grid container spacing={3}>
-      //           </Grid>
-      //         </form>
-      //       </CardContent>
-      //     </Card>
-
-      //     {/* Rewards Section */}
-      //     <Rewards />
-          
-      //     {/* GiftCard Section */}
-      //     <GiftCard />
-      //   </Grid>
-      // </Grid>
