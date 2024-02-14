79bd09e48efb520d6aa3a10a2f60ecffa583ed1d
linkwithlionhart <linkwithlionhart@gmail.com>
2024-02-13 22:18:18 -0800

    clean comments

--- src/components/profile/rewards/PointsCBar.jsx
+++ src/components/profile/rewards/PointsCBar.jsx
@@ -6,7 +6,7 @@ const PointsCBar = () => {
   const { points } = useRewards();
   const userId = window.sessionStorage.getItem('userId');
   const totalPoints = 100;
-  const progressPercent = Math.min((points / totalPoints) * 100, 100); // Ensure percentage does not exceed 100
+  const progressPercent = Math.min((points / totalPoints) * 100, 100);
 
   if (!userId) {
     return null;
@@ -17,31 +17,30 @@ const PointsCBar = () => {
       <Box position="relative" display="inline-flex">
         <CircularProgress
           variant="determinate"
-          value={100} // Full circle
-          size={100} // Adjust size as needed
-          thickness={4} // Adjust thickness as needed
+          value={100} 
+          size={100}
+          thickness={4}
           sx={{
-            color: 'rgba(0,0,0,0.1)', // Lighter color for the background
+            color: 'rgba(0,0,0,0.1)',
           }}
         />
         <CircularProgress
           variant="determinate"
           value={progressPercent}
-          size={100} // Adjust size as needed
-          thickness={4} // Adjust thickness as needed
+          size={100}
+          thickness={4} 
           sx={{
             position: 'absolute',
             left: 0,
-            color: 'rgba(0, 255, 0, 0.75)', // Custom color for the progress
-            // Adjustments to align with the background progress
+            color: 'rgba(0, 255, 0, 0.75)', 
             '& .MuiCircularProgress-circle': {
-              strokeLinecap: 'round', // Makes the line cap rounded
+              strokeLinecap: 'round',
             },
           }}
         />
       </Box>
       <Typography variant="subtitle1" sx={{ mt: 2 }}>
-        {`${points}/${totalPoints} points to your next reward!`}
+        {`${points}/${totalPoints} to redeem!`}
       </Typography>
     </Box>
   );
