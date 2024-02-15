449b42ff392776008b359eee3ad6f106af28fa4c
linkwithlionhart <linkwithlionhart@gmail.com>
2024-02-14 16:16:26 -0800

    implement red styling for negative integer values for points

--- src/components/profile/rewards/PointsHBar.jsx
+++ src/components/profile/rewards/PointsHBar.jsx
@@ -5,8 +5,13 @@ import { useRewards } from '../../../context/RewardsContext';
 const PointsHBar = () => {
   const { points } = useRewards();
   const userId = window.sessionStorage.getItem('userId');
-  const totalPoints = 500; 
-  const progressPercent = Math.min((points / totalPoints) * 100, 100); // Ensure percentage does not exceed 100
+  const totalPoints = 500;
+  
+  let progressPercent = Math.max(0, Math.min((points / totalPoints) * 100, 100));
+
+  const pointsMessage = points >= 0
+    ? `${points}/${totalPoints} points to your next reward!`
+    : `${points} points. Let's be positive about this.`;
 
   if (!userId) {
     return null;
@@ -14,21 +19,22 @@ const PointsHBar = () => {
 
   return (
     <Box display="flex" flexDirection="column" alignItems="center" p={2}>
-      <Box width="100%" mb={0} sx={{ borderRadius: 20, overflow: 'hidden' }}>
+      <Box maxWidth="400" width="100%" mb={0} sx={{ borderRadius: 20, overflow: 'hidden' }}>
         <LinearProgress 
-          variant="determinate" 
-          value={progressPercent} 
+          variant="determinate"
+          value={progressPercent}
           sx={{
             height: 10,
             borderRadius: 20,
             '& .MuiLinearProgress-bar': {
-              backgroundColor: 'rgba(0, 255, 0, 0.75)'
-            }
+              backgroundColor: points >= 0 ? 'rgba(0, 255, 0, 0.75)' : '#ff0000',
+            },
+            backgroundColor: points >= 0 ? 'rgba(0, 0, 0, 0.1)' : 'rgba(255, 0, 0, 0.5)',
           }}
         />
       </Box>
       <Typography variant="subtitle1">
-        {`${points}/${totalPoints} points to your next reward!`}
+        {pointsMessage}
       </Typography>
     </Box>
   );
